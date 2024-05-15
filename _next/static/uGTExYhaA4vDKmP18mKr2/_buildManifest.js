1c98b90523663557824fbd1ee1fa806de9fb11bd
watanabe-mizuki1 <watanabe-mizuki1@dmm.com>
2024-05-15 10:51:19 +0900

    chore: update [skip ci]

---
+++ _next/static/uGTExYhaA4vDKmP18mKr2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/4c9e6d934fe87f4d.css","static/chunks/pages/index-92f266784faecd83.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
